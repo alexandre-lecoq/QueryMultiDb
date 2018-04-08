c3c3365c7d331b1bc4d10d406bfe463b1f391beb
Alexandre Lecoq <alecoq@talentsoft.com>
2018-04-08 17:06:37 +0200

    Added logger name in excel log sheet.

--- QueryMultiDb/TableTarget.cs
+++ QueryMultiDb/TableTarget.cs
@@ -12,13 +12,14 @@ namespace QueryMultiDb
 
         public TableTarget()
         {
-            var logColumns = new TableColumn[6];
+            var logColumns = new TableColumn[7];
             logColumns[0] = new TableColumn("Id", typeof(int));
             logColumns[1] = new TableColumn("Date", typeof(string));
             logColumns[2] = new TableColumn("ThreadId", typeof(int));
             logColumns[3] = new TableColumn("Level", typeof(string));
-            logColumns[4] = new TableColumn("Message", typeof(string));
-            logColumns[5] = new TableColumn("Exception", typeof(string));
+            logColumns[4] = new TableColumn("Logger", typeof(string));
+            logColumns[5] = new TableColumn("Message", typeof(string));
+            logColumns[6] = new TableColumn("Exception", typeof(string));
 
             _logRows = new List<TableRow>();
             var logTable = new Table(logColumns, _logRows, Table.LogsId);
@@ -41,13 +42,14 @@ namespace QueryMultiDb
                 throw new ArgumentNullException(nameof(logEvent));
             }
 
-            var items = new object[6];
+            var items = new object[7];
             items[0] = logEvent.SequenceID;
             items[1] = logEvent.TimeStamp.ToString("o");
             items[2] = Thread.CurrentThread.ManagedThreadId; // System.Environment.CurrentManagedThreadId;
             items[3] = logEvent.Level;
-            items[4] = logEvent.Message;
-            items[5] = logEvent.Exception?.ToString() ?? "";
+            items[4] = logEvent.LoggerName;
+            items[5] = logEvent.Message;
+            items[6] = logEvent.Exception?.ToString() ?? "";
             var tableRow = new TableRow(items);
             _logRows.Add(tableRow);
         }
