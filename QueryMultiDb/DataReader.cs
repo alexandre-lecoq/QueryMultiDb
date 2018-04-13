alexandre-lecoq/QueryMultiDb

commit dfdb414b7291872ca029ed5c701bbe1390fd794e

Added ShowInformationMessages parameter.

QueryMultiDb/DataReader.cs
@@ -182,14 +182,17 @@ namespace QueryMultiDb
 
             var infoMessageRows = new List<TableRow>();
 
-            connection.FireInfoMessageEventOnUserErrors = true;
-
-            SqlInfoMessageEventHandler sqlInfoMessageEventHandler = (sender, arg) =>
+            if (Parameters.Instance.ShowInformationMessages)
             {
-                ConnectionOnInfoMessage(infoMessageRows, arg);
-            };
+                connection.FireInfoMessageEventOnUserErrors = true;
 
-            connection.InfoMessage += sqlInfoMessageEventHandler;
+                SqlInfoMessageEventHandler sqlInfoMessageEventHandler = (sender, arg) =>
+                {
+                    ConnectionOnInfoMessage(infoMessageRows, arg);
+                };
+
+                connection.InfoMessage += sqlInfoMessageEventHandler;
+            }
 
             var tableSet = new List<Table>();
 
@@ -230,15 +233,18 @@ namespace QueryMultiDb
                 Logger.Info($"{database.ToLogPrefix()} Records affected by query : {reader.RecordsAffected}");
             }
 
-            var infoMessageColumns = new TableColumn[6];
-            infoMessageColumns[0] = new TableColumn("Class", typeof(string));
-            infoMessageColumns[1] = new TableColumn("Number", typeof(string));
-            infoMessageColumns[2] = new TableColumn("State", typeof(string));
-            infoMessageColumns[3] = new TableColumn("Procedure", typeof(string));
-            infoMessageColumns[4] = new TableColumn("LineNumber", typeof(string));
-            infoMessageColumns[5] = new TableColumn("Message", typeof(string));
-            var informationMessageTable = new Table(infoMessageColumns, infoMessageRows, Table.InformationMessagesId);
-            tableSet.Add(informationMessageTable);
+            if (Parameters.Instance.ShowInformationMessages)
+            {
+                var infoMessageColumns = new TableColumn[6];
+                infoMessageColumns[0] = new TableColumn("Class", typeof(string));
+                infoMessageColumns[1] = new TableColumn("Number", typeof(string));
+                infoMessageColumns[2] = new TableColumn("State", typeof(string));
+                infoMessageColumns[3] = new TableColumn("Procedure", typeof(string));
+                infoMessageColumns[4] = new TableColumn("LineNumber", typeof(string));
+                infoMessageColumns[5] = new TableColumn("Message", typeof(string));
+                var informationMessageTable = new Table(infoMessageColumns, infoMessageRows, Table.InformationMessagesId);
+                tableSet.Add(informationMessageTable);
+            }
 
             var result = new ExecutionResult(database.ServerName, database.DatabaseName, tableSet);
 

QueryMultiDb/CommandLineParameters.cs
@@ -75,5 +75,9 @@ namespace QueryMultiDb
         /// XXX : Issue #42
         [Option(HelpText = "Show parameter sheet in excel file.", Default = false)]
         public bool ShowParameterSheet { get; set; }
+
+        /// XXX : Issue #42
+        [Option(HelpText = "Show parameter sheet in excel file.", Default = false)]
+        public bool ShowInformationMessages { get; set; }
     }
 }

QueryMultiDb/ExcelExporter.cs
@@ -300,7 +300,8 @@ namespace QueryMultiDb
                 CreateParameterRow("ShowLogSheet", parameters.ShowLogSheet),
                 CreateParameterRow("ShowParameterSheet", parameters.ShowParameterSheet),
                 CreateParameterRow("ShowServerName", parameters.ShowServerName),
-                CreateParameterRow("ShowDatabaseName", parameters.ShowDatabaseName)
+                CreateParameterRow("ShowDatabaseName", parameters.ShowDatabaseName),
+                CreateParameterRow("ShowInformationMessages", parameters.ShowInformationMessages)
             };
 
             var parameterTable = new Table(parameterColumns, parameterRows, Table.CommandLineParametersId);

QueryMultiDb/Parameters.cs
@@ -46,6 +46,8 @@ namespace QueryMultiDb
 
         public bool ShowParameterSheet { get; set; }
 
+        public bool ShowInformationMessages { get; set; }
+        
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Performance", "CA1812:AvoidUninstantiatedInternalClasses", Justification = "Used by JsonConvert")]
         private class JsonTargets
         {
@@ -168,9 +170,9 @@ namespace QueryMultiDb
             ShowNulls = parsedResult.ShowNulls;
             Progress = parsedResult.Progress;
             NullsColor = parsedResult.NullsColor;
-
             ShowLogSheet = parsedResult.ShowLogSheet;
             ShowParameterSheet = parsedResult.ShowParameterSheet;
+            ShowInformationMessages = parsedResult.ShowInformationMessages;
 
             ThrowIfInvalidParameter();
         }

commit fd77f03c459dea0da82acbcead96866199426d94

Fixed shamefully broken build in commit dfdb414b7291872ca029ed5c701bbe1390fd794e.

QueryMultiDb/DataReader.cs
@@ -182,15 +182,14 @@ namespace QueryMultiDb
 
             var infoMessageRows = new List<TableRow>();
 
+            SqlInfoMessageEventHandler sqlInfoMessageEventHandler = (sender, arg) =>
+            {
+                ConnectionOnInfoMessage(infoMessageRows, arg);
+            };
+
             if (Parameters.Instance.ShowInformationMessages)
             {
                 connection.FireInfoMessageEventOnUserErrors = true;
-
-                SqlInfoMessageEventHandler sqlInfoMessageEventHandler = (sender, arg) =>
-                {
-                    ConnectionOnInfoMessage(infoMessageRows, arg);
-                };
-
                 connection.InfoMessage += sqlInfoMessageEventHandler;
             }
 
@@ -248,7 +247,10 @@ namespace QueryMultiDb
 
             var result = new ExecutionResult(database.ServerName, database.DatabaseName, tableSet);
 
-            connection.InfoMessage -= sqlInfoMessageEventHandler;
+            if (Parameters.Instance.ShowInformationMessages)
+            {
+                connection.InfoMessage -= sqlInfoMessageEventHandler;
+            }
 
             return result;
         }