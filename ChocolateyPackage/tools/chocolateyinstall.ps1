b3c20932e3d4e9184731faa1f0054ead447bef97
Alexandre Lecoq <alecoq@talentsoft.com>
2018-05-23 07:22:29 +0200

    Added chocolatey package generation.

---
+++ ChocolateyPackage/tools/chocolateyinstall.ps1
@@ -0,0 +1,16 @@
+﻿
+$ErrorActionPreference = 'Stop';
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$fileLocation = Join-Path $toolsDir 'QueryMultiDb.msi'
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  fileType      = 'MSI'
+  file          = $fileLocation
+  softwareName  = 'QueryMultiDb*'
+  silentArgs    = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
+  validExitCodes= @(0, 3010, 1641)
+}
+
+Install-ChocolateyInstallPackage @packageArgs
