d143058ecbeba354651bf9867da65e9371ae10da
jiahao.tan <jiahao.tan@temus.com>
2022-09-03 01:43:19 +0800

    Updated to .net 6.0

--- QueryMultiDbGui/Program.cs
+++ QueryMultiDbGui/Program.cs
@@ -12,6 +12,7 @@ namespace QueryMultiDbGui
         private static void Main()
         {
             Application.EnableVisualStyles();
+            Application.SetHighDpiMode(HighDpiMode.SystemAware);
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new MainForm());
         }
